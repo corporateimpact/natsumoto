f50a16f86f88f0f39ef45f76a4ebb60d0b10b88a
horoiwa <horoiwa.yasuhiro@corporate.co.jp>
2020-08-22 23:28:56 +0900

    アメダススクレイピングファイル追加

---
+++ get_amedas_air_temp.php
@@ -0,0 +1,121 @@
+﻿<?php
+/* スクレーピング関数群のインクルード */
+include( "scrape_func.php" );
+
+/* 変数制限まとめて　使いまわし考慮の為出来るだけここを直すだけにする */
+$host_name = "127.0.0.1";
+$user_name = "root";
+$db_password = "pm#corporate1";
+$db_name = "natsumoto";
+$table_name = "air_temp";
+$area_url = "http://www.jma.go.jp/jp/amedas_h/today-33686.html?areaCode=000&groupCode=22";
+#$area_no = "1"; # 1 新町　2 釜石
+
+
+/* getURL()関数を使用して、ページの生データを取得する。 */
+$_rawData = getURL($area_url);
+
+
+/* 解析しやすいよう、生データを整理する。 */
+$_rawData = cleanString( $_rawData );
+//echo $_rawData;	//スクレイピングした内容を表示
+
+/* 次は若干ややこしい。　必要な項目の開始部分と終了部分は、事前に
+   HTMLから確認してある。　こういったものを利用して必要なデータを取得
+   する。 */
+/* 要素説明　"<td class=\"time left\">"　時間を特定するのに利用
+
+*/
+$_rawData = getBlock( "<td class=\"time left\">","</tr> </table>", $_rawData,false );
+// echo "getblockした後" . $_rawData;
+
+/* これで箇条書きに必要な特定データが入手できた。
+   ここでは項目を配列化した後、繰り返しで処理を行っている。 */
+$_rawData = explode( "</tr>", $_rawData );    //1時間毎に分割
+
+
+//取得するデータの時刻を求める。
+//$what=date( "H", time()+32400 );
+// タイムゾーンがずれているので修正:20200207 伊藤
+$what=date( "H", time() );
+$which=(int)$what;
+if($which==0)$which=24;
+
+
+//echo "<hr>";
+$now=0;            //時刻ではない
+/* 繰り返しを行いながら、個々の項目を解析する。 */
+foreach( $_rawData as $_rawBlock ) {
+    //初期化
+    //$time=null;
+    $temp=null;	//気温
+
+    //now=0:$which=1        //ひとつずれてる
+    if($now==($which)){
+        $_rawBlock = explode( "</td>", $_rawBlock );
+        $str="";
+        $null_count=0;
+        $num_element=count($_rawBlock)-1;
+        for($j=0;$j<$num_element;$j++){
+            $_rawBlock[$j]=strip_tags($_rawBlock[$j]);                //余計なタグを除去
+            $_rawBlock[$j] = trim( $_rawBlock[$j] );                  //空白を除去
+            //echo $_rawBlock[$j];
+
+            if(!strcmp($_rawBlock[$j],"&nbsp;")){                     //"&nbsp;"=空白
+                $_rawBlock[$j]=0;                                     //"&nbsp;"のとき、0を代入
+                $null_count++;
+            }
+            if($j==0){
+                //時間
+                //$time=$_rawBlock[$j];
+                //if($time==24)$time=0;    //24時 => 0時
+            }
+            else if($j==1){
+                //気温（℃）
+                $temp=$_rawBlock[$j];
+                $str.=$temp;
+            }
+        }
+        if($null_count==$num_element-1){
+            //echo "null!!";
+        }
+        $sc_time = $_rawBlock[0];    //取得時刻
+        $sc_temp = $_rawBlock[1];    //気温
+        $sc_rain = $_rawBlock[2];    //時間降水量
+        $sc_wind = $_rawBlock[3];    //風量
+        $sc_wspd = $_rawBlock[4];    //風速
+        #$sc_sun  = $_rawBlock[5];    //日照量
+
+
+        //データ成形
+        $sc_time = $sc_time . ":00:00";
+        $sc_date = date('Y-m-d');
+        echo "\n" . $sc_time . "\n";
+        $sc_date = '"' . $sc_date . '"';
+        $sc_time = '"' . $sc_time . '"';
+
+        //24時→0時として登録する
+        $sc_time = str_replace("24", "00", $sc_time);
+        echo $sc_time . "\n";
+
+        //ＭｙＳＱＬへ接続（DB_HOST, DB_USER, DB_PASS）
+        $mysqli = new mysqli ($host_name, $user_name, $db_password, $db_name);
+        if ($mysqli->connect_error) {
+            echo $mysqli->connect_error;
+            exit();
+        }
+        else {
+            $mysqli->set_charset("utf8");
+        }
+
+        //mysql構文4 データ登録用
+        $sql = "replace into area_info values ( " .$sc_date . ", " . $sc_time . (float)$sc_temp . ");";
+        $mysqli_result = $mysqli->query($sql);
+        if (!$mysqli_result) {
+            die('insert fault'.mysql_error() . "\n");
+        }
+        $mysqli->close();        //DB.close();
+    }
+    $now++;
+}
+?>
---
+++ scrape_func.php
@@ -0,0 +1,48 @@
+﻿x<?php
+function getURL( $pURL ) {
+$_data = null;
+if( $_http = fopen( $pURL, "r" ) ) {
+while( !feof( $_http ) ) {
+$_data .= fgets( $_http,1024 );
+}
+fclose( $_http );
+}
+return( $_data );
+}
+
+
+
+function cleanString( $pString ) {
+$_data = str_replace( array( chr(10), chr(13), chr(9) ), chr(32), $pString );
+while( strpos( $_data, str_repeat( chr(32), 2 ), 0 ) != false ) {
+$_data = str_replace( str_repeat( chr(32), 2 ),chr(32), $_data );
+}
+return( trim( $_data ) );
+}
+
+
+
+function getBlock( $pStart, $pStop, $pSource, $pPrefix = true ) {
+$_data = null;
+$_start = strpos( strtolower( $pSource ), strtolower( $pStart ), 0 );
+$_start = ( $pPrefix == false ) ? $_start + strlen( $pStart ) : $_start;
+$_stop = strpos( strtolower( $pSource ), strtolower( $pStop ), $_start );
+//if($_start > strlen( $pElement ) && $_stop > $_start ){
+if ($_start > strlen($pStart) && $_stop > $_start) {
+$_data = trim( substr( $pSource, $_start, $_stop - $_start ));
+}
+return( $_data );
+}
+
+function getElement( $pElement, $pSource ) {
+$_data = null;
+$pElement = strtolower( $pElement );
+$_start = strpos( strtolower( $pSource ), chr(60) . $pElement, 0 );
+$_start = strpos( strtolower( $pSource ), "</" . $pElement . chr(62), $_start );
+//if( $_start > strlen( $pElement ) && $_stop > $_start ) {
+if(isset($pElement) && $_start > strlen( $pElement ) && $_stop > $_start ){
+$_data = trim( substr( $pSource, $_start, $_stop - $_start ) );
+}
+return( $_data );
+}
+?>
