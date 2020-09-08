eb2584c2dc544020cd102e87635e891668a19ea7
Ch1kuw4 <itou.shou@corporate.co.jp>
2020-09-08 11:40:07 +0900

    onClickのスペルミス修正

--- graph.php
+++ graph.php
@@ -411,7 +411,7 @@ $mysqli->close();
         </div>
 
     <form method="post" name="aForm">
-        <input type="button" value="グラフデータダウンロード" onclick="onDownload();"> <input type="text" name="date_from" id="xxdate2" readonly="readonly" value="<?php echo $org_date; ?>"> ～ <input type="text" name="date_to" id="xxdate3" readonly="readonly" value="<?php echo $org_date; ?>">
+        <input type="button" value="グラフデータダウンロード" onClick="onDownload();"> <input type="text" name="date_from" id="xxdate2" readonly="readonly" value="<?php echo $org_date; ?>"> ～ <input type="text" name="date_to" id="xxdate3" readonly="readonly" value="<?php echo $org_date; ?>">
     </form>
     <br>
     <?php echo $org_date; ?>
